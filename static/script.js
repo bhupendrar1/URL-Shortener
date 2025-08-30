14c2aa146a166ffa43cd42454860c663094c8635
Bhupendra Singh <bs4004908@gmail.com>
2025-08-30 17:06:29 +0530

    main file

---
+++ static/script.js
@@ -0,0 +1,13 @@
+/**
+
+ */
+$(function () {
+    $('#submit').click(function () {
+        var url = $('#url').val();
+        $.post('/api/v1/shorten', {
+            url:url
+        },function (data) {
+            $('#shortcode').html("Chotta URL: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
+        })
+    })
+})
